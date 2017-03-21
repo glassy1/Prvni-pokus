ffeaefabc5e4a93bd373a44f3531ef5208fd2490
glassy1 <t.glasberger@seznam.cz>
2017-03-21 11:18:27 +0100

    Pridana kontrola znaku a quit

--- pokus.py
+++ pokus.py
@@ -1,4 +1,5 @@
 import sys
+from msvcrt import getch
 
 def printXY(progress):
     """ahoj"""
@@ -8,5 +9,11 @@ def printXY(progress):
     sys.stdout.write("Download progress: %d%%\r" %(progress)) # to umoznuje zapis do jedne radky. Nejak je mozno nastavit i sloupce. Nuntou prozkoumat. Mozno bude lepsi pouzit Tkinter
     sys.stdout.flush()
 
-for i in range(1000):
-    printXY(i) # vytiskni dane cislo 
+for i in range(10):
+    printXY(i) # vytiskni dane cislo
+print
+key="" # zde se uklada stisknuta klavesa
+while key!="q": # cekej na stisk klavesy
+    print "Stiskni 'q'  pro ukonceni programu."
+    key = getch()
+    print(key)
