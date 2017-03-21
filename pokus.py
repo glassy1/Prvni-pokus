d14a0cc3bfad9c52f729976e7038665cea9f40ba
glassy1 <t.glasberger@seznam.cz>
2017-03-21 11:47:30 +0100

    Pridano oznaceni zacatku a konce vykonavani programu

--- pokus.py
+++ pokus.py
@@ -4,16 +4,25 @@ from msvcrt import getch
 def printXY(progress):
     """ahoj"""
     a=0
-    for i in range(100000):
+    for i in range(10000):
         a=a*5
     sys.stdout.write("Download progress: %d%%\r" %(progress)) # to umoznuje zapis do jedne radky. Nejak je mozno nastavit i sloupce. Nuntou prozkoumat. Mozno bude lepsi pouzit Tkinter
     sys.stdout.flush()
-
-for i in range(10):
-    printXY(i) # vytiskni dane cislo
 print
+print "==================================="
+print "                Start              "
+print "==================================="
+for i in range(100):
+    printXY(i) # vytiskni dane cislo
+
+print
+
 key="" # zde se uklada stisknuta klavesa
 while key!="q": # cekej na stisk klavesy
     print "Stiskni 'q'  pro ukonceni programu."
     key = getch()
     print(key)
+
+print "==================================="
+print "                Konec              "
+print "==================================="
